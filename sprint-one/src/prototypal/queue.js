39c7e7cb9b2e30416edf44c74a371ef2d7ff9504
nrobeR <mr.nrober@gmail.com>
2014-07-24 10:28:25 -0700

    finished prototypal classes

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
-var makeQueue = function() {
+var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance = {
+    'head':0,
+    'tail':0,
+    'storage':{}
+  };
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.tail] = value;
+    this.tail++;
+  },
+
+  dequeue: function(){
+    if(this.tail>this.head){
+      var result = this.storage[this.head];
+      delete this.storage[this.head];
+      this.head++;
+    }
+    return result;
+  },
+
+  size: function(){
+    return this.tail-this.head;
+  }
+};
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
-var makeStack = function() {
+ var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-};
+  // but try not not reference your old code in writing the new style
+  var someInstance = Object.create(stackMethods);
+  someInstance.counter = 0;
+  someInstance.storage = {};
 
-var stackMethods = {};
+  return someInstance;
+
+};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+
+  pop: function(){
+    this.counter && this.counter--;
+    var result = this.storage[this.counter];
+    delete this.storage[this.counter];
+    return result;
+  },
+
+  size: function(){
+    return this.counter;
+  }
+};
 
 
