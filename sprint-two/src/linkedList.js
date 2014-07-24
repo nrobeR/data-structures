7d415e26cd64f617fce56dc416d0789e2c92f2de
nrobeR <mr.nrober@gmail.com>
2014-07-24 15:14:49 -0700

    got linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if(!list.head && !list.tail){
+      list.head = node;
+      list.tail = node;
+    }else{
+      list.tail.next = node;
+      list.tail = node;
+    }
+
   };
 
   list.removeHead = function(){
+    var temp = list.head;
+    if(list.head.next){
+      list.head = list.head.next;
+    }else{
+      list.head = null;
+      list.tail = null;
+    }
+    return temp.value;
   };
 
   list.contains = function(target){
+    var result = false;
+    var currentnode = list.head;
+    while(currentnode){
+      if(currentnode.value === target){
+        result = true;
+      }
+      currentnode = currentnode.next;
+    }
+    return result;
   };
 
   return list;
