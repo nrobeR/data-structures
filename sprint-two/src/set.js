dc9b643d4befff5fb494e42bcdedd6fafad1b472
nrobeR <mr.nrober@gmail.com>
2014-07-24 16:21:26 -0700

    at hashtable

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,36 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {}; //'undefined' originally dont forget bro.
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+
+this._storage[item] = item;
+
+
 };
 
 setPrototype.contains = function(item){
+if(this._storage[item] === item)
+{
+  return true;
+}
+else
+{
+  return false;
+}
 };
 
 setPrototype.remove = function(item){
+if(this._storage[item] = item)
+{
+
+  this._storage[item] = undefined;
+}
+
 };
 
 /*
